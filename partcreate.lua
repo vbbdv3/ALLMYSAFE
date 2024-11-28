54969ce1c7ed0a4f66196d79daa9f08c80af3548
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-11-29 01:58:25 +0500

    Create partcreate.lua

---
+++ partcreate.lua
@@ -0,0 +1,7 @@
+local part = Instance.new("Part")
+part.Name = "JurassicPart"
+part.Anchored = true
+part.Size = Vector3.new(10, 1, 10)
+part.Color = Color3.new(1, 1, 1)
+part.Position = Vector3.new(-2069.291259765625, 16.463865280151367, -2266.935546875)
+part.Parent = workspace
